2024ef0ce108aa824bad2c4a9546c5d5b1801a88
Ganon1998 <jgreene097@gmail.com>
2020-12-23 13:12:01 -0500

    Add files via upload

---
+++ ISIC_Challenge.py
@@ -0,0 +1,126 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import matplotlib.pyplot as plt
+import math as math
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.metrics import roc_curve
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# download CUDA 10.1
+#physical_device = tf.config.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(physical_device[0], True)
+
+train_examples = 20225
+test_examples = 2551
+validation_examples = 2555
+img_height = img_width = 224
+batch_size = 32
+
+model = keras.Sequential([
+    hub.KerasLayer("https://tfhub.dev/google/imagenet/nasnet_mobile/feature_vector/4", trainable=True),
+    layers.Dense(1, activation="sigmoid"),
+])
+
+train_datagen = ImageDataGenerator(
+    rescale=1.0 / 255,
+    rotation_range=15,
+    zoom_range=(0.95, 0.95),
+    horizontal_flip=True,
+    vertical_flip=True,
+    data_format="channels_last",
+    dtype=tf.float32,  # no validation split here cuz all of this data augmentation will be applied to the
+    # validation set....big no no
+)
+
+validation_datagen = ImageDataGenerator(rescale=1.0 / 255, dtype=tf.float32)
+test_datagen = ImageDataGenerator(rescale=1.0 / 255, dtype=tf.float32)
+
+# training
+train_gen = train_datagen.flow_from_directory(
+    "D:/data/train",
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    color_mode="rgb",
+    class_mode="binary",
+    shuffle=True,
+    seed=123,
+)
+
+# validation
+validation_gen = validation_datagen.flow_from_directory(
+    "D:/data/validation",
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    color_mode="rgb",
+    class_mode="binary",
+    shuffle=True,
+    seed=123,
+)
+
+# test
+test_gen = test_datagen.flow_from_directory(
+    "D:/data/test",
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    color_mode="rgb",
+    class_mode="binary",
+    shuffle=True,
+    seed=123,
+)
+
+# accuracy here will be a flawed metric cuz the classes are imbalanced and the data is imbalanced
+# in otherwords, 10% of the photos could be cancer while the other 90% could be skin lesions
+# With that said, we'll have to use a different metric using precision, recall and auc and accuracy
+METRICS = [
+    keras.metrics.BinaryAccuracy(name="accuracy"),
+    keras.metrics.Precision(name="precision"), # computes precision of predictions w/ regards to the label
+    keras.metrics.Recall(name="recall"),# computes recall: examines true positive, false negative, false_positives, and true_negatives and calls combined with sample weights
+    keras.metrics.AUC(name="auc"), # Area Under Curve via Riemann sum: The area under the ROC-curve is therefore computed using the height of the recall values by the false positive rate, while the area under the PR-curve is the computed using the height of the precision values by the recall.
+]
+
+model.compile(
+    optimizer=keras.optimizers.Adam(lr=3e-4),
+    loss=[keras.losses.BinaryCrossentropy(from_logits=False)],
+    metrics=METRICS
+)
+
+model.fit(
+    train_gen,
+    epochs=15,
+    steps_per_epoch=train_examples // batch_size,
+    validation_data=validation_gen,
+    validation_steps=validation_examples,
+    #callbacks=[keras.callbacks.ModelCheckpoint("cancer_detection_model")] will save a model every epoch
+)
+
+# calculate true positive and false positive rate
+def plot_roc(labels, data):
+    predictions = model.predict(data)
+    fp, tp, _ = roc_curve(labels, predictions)
+
+    plt.plot(100*fp, 100*tp)
+    plt.xlabel("False Positive in %")
+    plt.ylabel("True Positives in %")
+    plt.show()
+
+test_labels = np.array([])
+num_batches = 0
+
+for _, y in test_gen:
+    test_labels = np.append(test_labels, y)
+    num_batches += 1
+    if num_batches == math.ceil(test_examples / batch_size):
+        break
+
+# give us ROC curve on the graph
+plot_roc(test_labels, test_gen)
+
+model.evaluate(validation_gen, verbose=2)
+model.evaluate(test_gen, verbose=2)
+
+model.save_weights('saved_model')
---
+++ MakeDir_Script.py
@@ -0,0 +1,48 @@
+seed = 1
+random.seed(seed)
+directory = "D:/ISIC/images/"
+train = "D:/data/train/"
+test = "D:/data/test/"
+validation = "D:/data/validation/"
+
+os.makedirs(train + "benign/")
+os.makedirs(train + "malignant/")
+os.makedirs(test + "benign/")
+os.makedirs(test + "malignant/")
+os.makedirs(validation + "benign/")
+os.makedirs(validation + "malignant/")
+
+test_examples = train_examples = validation_examples = 0
+
+# open labels file and reads from the 1st row on
+for line in open("D:/ISIC/ISIC_2019_Training_GroundTruth.csv").readlines()[1:]:
+    split_line = line.split(",")
+    img_file = split_line[0]
+    benign_malignant = split_line[1]
+
+    # this will be used to determine how many test and validation sets should have
+    random_num = random.random()
+
+    if random_num < 0.8:
+        location = train
+        train_examples +=1
+
+    elif random_num < 0.9:
+        location = validation
+        validation_examples +=1
+
+    else:
+        location = test
+        test_examples += 1
+
+
+    if int(float(benign_malignant)) == 0:
+        shutil.copy(
+            "D:/ISIC/images/ISIC_2019_Training_Input/" + img_file + ".jpg",
+            location + "benign/" + img_file + ".jpg",
+        )
+    elif int(float(benign_malignant)) == 1:
+        shutil.copy(
+            "D:/ISIC/images/ISIC_2019_Training_Input/" + img_file + ".jpg",
+            location + "malignant/" + img_file + ".jpg",
+        )
